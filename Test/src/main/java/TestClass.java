3edf5e473132382c024664bbc56d2f9f59070dbf
KilianMcG <91564236+KilianMcG@users.noreply.github.com>
2021-11-03 19:12:27 +0000

    Testing the repo

---
+++ Test/src/main/java/TestClass.java
@@ -0,0 +1,11 @@
+public class TestClass {
+    int num;
+
+    public TestClass(int newNum){
+        num = newNum;
+    }
+
+    public void printNum(int othernum){
+        System.out.println(num + " and " + othernum);
+    }
+}
